17774a73a944aa010799e3f7b8a1de3e75100b61
slamaslo <snlamas@gmail.com>
2025-04-24 18:59:49 -0600

    feat: add functionality to create new book entries via form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Bookshelf from "./components/Bookshelf/Bookshelf.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-
+      <h1>My Bookshelf</h1>
+      <Bookshelf />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
